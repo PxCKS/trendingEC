997ff448c2e721612ffdd5f07935aa7f897aaa4e
SivuyiseNcapayi <sivuyisencapai@gmail.com>
2024-06-11 15:53:01 +0200

    Initial login_feature: Added the login logic, and comments, added some debug statement to test the function

--- demo.py
+++ demo.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, url_for, request,redirect
 import mysql.connector
 
-
+#making the instance of the Flask application
 app = Flask(__name__)
 
+#function to make connection to the database
 def get_db_connection():
     conn = mysql.connector.connect(
         host="localhost",
@@ -13,20 +14,52 @@ def get_db_connection():
     )
     return conn
 
+#Function used to define home page logic
 @app.route('/')
 def home():
     return render_template('index.html')
 
+#function used to handle login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
+    #checking the http method
     if request.method == 'POST':
-        username=request.form.get('username')
-        email=request.form.get('email')
-        password=request.form.get('password')
 
-        sql="SELECT * FROM USERS"
+        #getting input values from the html pages
+        username = request.form.get('username')
+        password = request.form.get('password')
 
-        curso
+        #checking for empty input fields
+        if not username or not password:
+            return "All fields are required", 400
+        #making a connection to the database and creating a cursor object
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        
+        #sql query for checking the table in the database
+        sql = "SELECT * FROM USERS WHERE username = %s AND password = %s"
+        param = (username, password)
+        cursor.execute(sql, param)
+        
+
+        
+        user = cursor.fetchone()
+        #closing connection and cursor
+
+        print(f"User fetched: {user}")
+        cursor.close()
+        conn.close()
+
+        #checking if the specified credential exist in the database
+        if user:
+            app.logger.info(f"Successful login attempt for user: {username}")
+            return redirect(url_for('home'))
+        
+        else:
+            app.logger.warning(f"Failed login attempt for user: {username}")
+            return "Invalid Credentials.", 401
+
     return render_template("login.html")
 
 @app.route('/signup', methods=['GET', 'POST'])
